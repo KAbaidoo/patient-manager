e10121ac5d3466112c89a22cd0a5d83de7c51699
Kelvin Abaidoo <kabaidoo@hotmail.com>
2022-08-09 10:34:48 +0000

    Edited README

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-# health-app-2
-1. npm install
-2. open on local host port 4000
+# Patient Management System
 
+A fully functional application for managing patient information at a clinic.
+
+## Description
+
+Before transitioning into tech I worked in healthcare, this was my motivation to create this application for managing patient information. Technologies used include [MongoDB](https://www.mongodb.com/), [NodeJS](https://nodejs.org/en/), [ExpressJS](https://expressjs.com/), [Bootstrap](https://getbootstrap.com/) and [EJS](https://ejs.co/). The application includes validation for users.
+
+## Installation
+
+- Clone the repository
+
+```
+npm install
+npm start
+```
+
+- Access on `http://localhost:4000`
+
+## Usage
+
+- The system requires users to be register and log in before usage.
+- A new patient can be registered with basic information.
+- Upon selecting the patient from a list, more information about the patient become visible.
+- A user can edit patient information.
+- A user can add payments made by a patient.
+
+## Further Work
+
+- More fields can be added to store more patient information.
+- Implement proper dialog messages.
--- models/patients.js
+++ models/patients.js
@@ -1,34 +1,35 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
-//  Article
+//  Patients
 let patientsSchema = mongoose.Schema({
-    fname:{
-        type: String,
-        required: true
-    },
-    lname:{
-        type: String,
-        required: true
-    },
-    gender:{
-        type: String,
-        required: true
-    },
-    birthdate:{
-        type: Date,
-        required: true
-    },
-    address:{
-        type: String,
-        required: true
-    },
-    phone:{
-        type: String,
-        required: true
-    },
-    payments:{
-         type: Number, default: 0
-    }
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  birthdate: {
+    type: Date,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  payments: {
+    type: Number,
+    default: 0,
+  },
 });
 
-module.exports =  mongoose.model('Patients', patientsSchema);
+module.exports = mongoose.model("Patients", patientsSchema);
